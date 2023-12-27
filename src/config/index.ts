54df7aa1d000c36a8cdff6f8526b691215a8e6fe
Your Name <you@example.com>
2023-12-27 00:25:03 +0000

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const COACH_CATEGORIES = [
+  {
+    label: "Coaches",
+    value: "Coaches" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Training Plans",
+    value: "training_plans" as const,
+    featured: [
+      {
+        name: "Favorite icon picts",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Best selling Training Plans",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+]
